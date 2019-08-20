93b0e828615fc30a5716786e245b5ceed22ef3e7
Maikelb <maikel.bolderdijk@student.hu.nl>
2019-08-20 16:55:21 +0200

    Changed root route

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});
+Route::get('/', 'PostController@index');
 
 Route::get('/posts', 'PostController@index');
 Route::get('/posts/single/{id}', 'PostController@singlePost');
